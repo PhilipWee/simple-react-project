5e8f317a010628173aa858241b407347345d57c5
Philip Andrew Wee De Wang <41712275+PhilipWee@users.noreply.github.com>
2023-07-05 13:22:51 +0800

    Update by Tweak Wed Jul 05 2023 13:22:51 GMT+0800 (Singapore Standard Time)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+    (<div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
       <img
         src="/img/beams.jpg"
         alt=""
@@ -13,10 +13,8 @@ function App() {
           <img src="/img/logo.svg" className="h-6" alt="Tailwind Play" />
           <div className="divide-y divide-gray-300/50">
             <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
-              <p>
-                An advanced online playground for Tailwind CSS, including
-                support for things like:
-              </p>
+              <p>An advanced online playground for Tailwind CSS, including
+                                support for things like:</p>
               <ul className="space-y-4">
                 <li className="flex items-center">
                   <svg
@@ -30,12 +28,21 @@ function App() {
                       fill="none"
                     />
                   </svg>
-                  <p className="ml-4">
-                    Customizing your
-                    <code className="text-sm font-bold text-gray-900">
-                      tailwind.config.js
-                    </code>{" "}
-                    file
+                  <p className="ml-4">Customizing your<code className="text-sm font-bold text-gray-900">tailwind.config.js</code>{" "}file</p>
+                </li>
+                <li className="flex items-center">
+                  <svg
+                    className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="11" />
+                    <path
+                      d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                      fill="none"
+                    />
+                  </svg>
+                  <p className="ml-4">Extracting classNamees with<code className="text-sm font-bold text-gray-900">@apply</code>
                   </p>
                 </li>
                 <li className="flex items-center">
@@ -50,32 +57,11 @@ function App() {
                       fill="none"
                     />
                   </svg>
-                  <p className="ml-4">
-                    Extracting classNamees with
-                    <code className="text-sm font-bold text-gray-900">
-                      @apply
-                    </code>
-                  </p>
-                </li>
-                <li className="flex items-center">
-                  <svg
-                    className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <circle cx="12" cy="12" r="11" />
-                    <path
-                      d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
-                      fill="none"
-                    />
-                  </svg>
-                  <p className="ml-4">Code completion with instant preview</p>
+                  <p className="ml-4">Code completion with instant preview WOW</p>
                 </li>
               </ul>
-              <p>
-                Perfect for learning how the framework works, prototyping a new
-                idea, or creating a demo to share online.
-              </p>
+              <p>Perfect for learning how the framework works, prototyping a new
+                                idea, or creating a demo to share online.</p>
             </div>
             <div className="pt-8 text-base font-semibold leading-7">
               <p className="text-gray-900">Want to dig deeper into Tailwind?</p>
@@ -83,15 +69,13 @@ function App() {
                 <a
                   href="https://tailwindcss.com/docs"
                   className="text-sky-500 hover:text-sky-600"
-                >
-                  Read the docs &rarr;
-                </a>
+                >Read the docs →</a>
               </p>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </div>)
   );
 }
 
