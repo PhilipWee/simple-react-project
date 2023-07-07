f3543abae44c9802883d502a7c424198e200df03
StephenAlvin <49267794+StephenAlvin@users.noreply.github.com>
2023-07-07 16:57:49 +0800

    Update by Tweak Fri Jul 07 2023 16:57:49 GMT+0800 (Singapore Standard Time)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+    (<div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
       <img
         src="/img/beams.jpg"
         alt=""
@@ -8,15 +8,12 @@ function App() {
         width="1308"
       />
       <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-      <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+      <div className="[backgroundColor:#bfbfbf] relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
         <div className="mx-auto max-w-md">
           <img src="/img/logo.svg" className="h-6" alt="Tailwind Play" />
           <div className="divide-y divide-gray-300/50">
             <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
-              <p>
-                An advanced online playground for Tailwind CSS, including
-                support for things like:
-              </p>
+              <p>Tweak is an awesome tool for designers</p>
               <ul className="space-y-4">
                 <li className="flex items-center">
                   <svg
@@ -30,13 +27,7 @@ function App() {
                       fill="none"
                     />
                   </svg>
-                  <p className="ml-4">
-                    Customizing your
-                    <code className="text-sm font-bold text-gray-900">
-                      tailwind.config.js
-                    </code>{" "}
-                    file
-                  </p>
+                  <p className="ml-4">Customizing your<code className="text-sm font-bold text-gray-900">tailwind.config.js</code>{" "}file</p>
                 </li>
                 <li className="flex items-center">
                   <svg
@@ -50,11 +41,7 @@ function App() {
                       fill="none"
                     />
                   </svg>
-                  <p className="ml-4">
-                    Extracting classNamees with
-                    <code className="text-sm font-bold text-gray-900">
-                      @apply
-                    </code>
+                  <p className="ml-4">Extracting classNamees with<code className="text-sm font-bold text-gray-900">@apply</code>
                   </p>
                 </li>
                 <li className="flex items-center">
@@ -72,10 +59,8 @@ function App() {
                   <p className="ml-4">Code completion with instant preview</p>
                 </li>
               </ul>
-              <p>
-                Perfect for learning how the framework works, prototyping a new
-                idea, or creating a demo to share online.
-              </p>
+              <p>Perfect for learning how the framework works, prototyping a new
+                                idea, or creating a demo to share online.</p>
             </div>
             <div className="pt-8 text-base font-semibold leading-7">
               <p className="text-gray-900">Want to dig deeper into Tailwind?</p>
@@ -83,15 +68,13 @@ function App() {
                 <a
                   href="https://tailwindcss.com/docs"
                   className="text-sky-500 hover:text-sky-600"
-                >
-                  Read the docs &rarr;
-                </a>
+                >Read the docs →</a>
               </p>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </div>)
   );
 }
 
