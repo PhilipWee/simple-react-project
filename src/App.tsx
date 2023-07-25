a3d80b5f983fe421bc6640028efbd8a2fd127aee
Philip Andrew Wee De Wang <41712275+PhilipWee@users.noreply.github.com>
2023-07-25 15:36:56 +0800

    Update by Tweak Tue Jul 25 2023 15:36:55 GMT+0800 (Singapore Standard Time)

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
               <MyNiceLittleComponent text="So Brave" />
               <MyNiceLittleComponent
                 text="So Brave"
-                subText="This should be small"
+                subText="This sHeyeyeeyey"
               />
               <ul className="space-y-4">
                 <li className="flex items-center">
